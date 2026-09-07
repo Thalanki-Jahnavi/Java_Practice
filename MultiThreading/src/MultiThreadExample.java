673ac468e0c27d27d41f65c1a6c43ebb665f086f
Thalanki-Jahnavi <jahnavithalanki1827@gmail.com>
2026-09-07 11:03:41 +0530

    added multi-threading example

---
+++ MultiThreading/src/MultiThreadExample.java
@@ -0,0 +1,8 @@
+// Thread[main,5,main]
+
+public class MultiThreadExample {
+    public static void main(String[] args) {
+        Thread t= Thread.currentThread();
+        System.out.println(t);
+    }
+}
